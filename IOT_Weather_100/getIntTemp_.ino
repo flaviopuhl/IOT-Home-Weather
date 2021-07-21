42fd88b2f2d29e4de4329020722de75e7237b73b
Flávio Luiz Puhl Junior <33373207+flaviopuhl@users.noreply.github.com>
2021-07-21 09:15:38 -0300

    Add files via upload

---
+++ IOT_Weather_100/getIntTemp_.ino
@@ -0,0 +1,15 @@
+void getIntTemp(){
+
+  char buff[10];
+  
+  sensors.requestTemperatures();                            // get Temperature from DS18B20
+  float_tempC = sensors.getTempCByIndex(0);
+    
+    dtostrf(float_tempC, 2, 1, buff);  //2 is mininum width, 1 is precision
+      string_tempC = buff;
+      
+  Serial.println("");
+  Serial.println("################# INT Temp #################\n");
+  Serial.print("Internal Temp °C  : "); Serial.println(float_tempC);
+  
+}
