c01957b105ef8db1d4f54a97610a87e2d52c35f1
Flávio Luiz Puhl Junior <33373207+flaviopuhl@users.noreply.github.com>
2021-07-21 09:21:51 -0300

    Add files via upload

--- displayScreen1_.ino
+++ displayScreen1_.ino
@@ -2,9 +2,16 @@ void Screen_Actual(){
 
 display.clear();
 
+         display.setTextAlignment(TEXT_ALIGN_LEFT);
+      display.setFont(ArialMT_Plain_10);
+        display.drawString(0, 0, currentTime);
+
+        display.setTextAlignment(TEXT_ALIGN_RIGHT);
+      display.setFont(ArialMT_Plain_10);
+        display.drawString(128, 0, currentDate); 
+        
         display.setTextAlignment(TEXT_ALIGN_LEFT);
       display.setFont(ArialMT_Plain_10);
-        display.drawString(0, 0, "Atual");
         display.drawString(0, 13, "EXTERNO");
         display.drawString(90, 13, "FEELS");
       
@@ -15,8 +22,6 @@ display.clear();
         display.drawString(68, 43, "°C");                         
 
       display.setTextAlignment(TEXT_ALIGN_RIGHT);
-        display.setFont(ArialMT_Plain_10);
-        display.drawString(128, 0, currentDate);
          display.setFont(Dialog_plain_16);
         display.drawString(128, 25, Current_feels_like+ " ºC");
       
